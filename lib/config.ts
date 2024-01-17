8c7338aa30bf9f2344c5afba08c92ec5f191aad9
lxy-yz <lxy.dev.yz@gmail.com>
2024-01-17 13:55:59 -0800

    fix: social image and remove puppeteer

--- lib/config.ts
+++ lib/config.ts
@@ -125,12 +125,16 @@ export const isServer = typeof window === 'undefined'
 
 export const port = getEnv('PORT', '3000')
 export const host = isDev ? `http://localhost:${port}` : `https://${domain}`
+export const apiHost = isDev
+  ? host
+  : `https://${process.env.VERCEL_URL || domain}`
 
 export const apiBaseUrl = `/api`
 
 export const api = {
   searchNotion: `${apiBaseUrl}/search-notion`,
-  getSocialImage: `${apiBaseUrl}/social-image`
+  getSocialImage: `${apiBaseUrl}/social-image`,
+  getNotionPageInfo: `${apiBaseUrl}/notion-page-info`
 }
 
 // ----------------------------------------------------------------------------
